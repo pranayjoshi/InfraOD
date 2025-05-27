6cbc3acf8282f7cff5b0c68724c6abba5225ae9b
Pranay Joshi <pjoshi6@crimson.ua.edu>
2025-05-27 18:58:46 -0500

    Readme updated

--- src/train.py
+++ src/train.py
@@ -1,12 +1,14 @@
-from ultralytics import YOLO
+# Example of training code
 
-if __name__ == "__main__":
-    model = YOLO('yolo11n.pt')
-    results = model.train(
-        data='dataset.yaml',
-        imgsz=336,
-        epochs=50,
-        batch=16,
-        name='InfraOD_model'
-    )
-    print("Training complete. Results:", results)
+# from ultralytics import YOLO
+
+# if __name__ == "__main__":
+#     model = YOLO('yolo11n.pt')
+#     results = model.train(
+#         data='dataset.yaml',
+#         imgsz=336,
+#         epochs=50,
+#         batch=16,
+#         name='InfraOD_model'
+#     )
+#     print("Training complete. Results:", results)
